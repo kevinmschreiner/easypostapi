f1e8a714c67ca8b9d12b2daadd8289ce3a0ed060
Kevin Schreiner <kschreiner@nationsphotolab.com>
2020-05-08 16:04:41 -0400

    Added Event model

---
+++ Library/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPost.Light.Library
+{
+    public class Event : BaseDefinition
+    {
+        public string mode;
+        public string description;
+        public object previous_attributes;
+        public object result;
+        public string status;
+        public string[] pending_urls;
+        public string[] completed_urls;
+    }
+}
